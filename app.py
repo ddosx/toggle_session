01292cba40cd0ca07d52f2e6917ddec322b61fba
Vova Kolinko <vovakolinko@gmail.com>
2022-01-22 16:27:42 +0300

    Файл app.py

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
-import os
 import libs
 
+libs.createtmppath()
+status = libs.checktempfile()
+status = libs.returnstatus(status)
+status = libs.toggletempfile(status)
+status = libs.togglescreen(status)
