69cce83c0ffc527c35fa8d21203f3e9227a05b10
Vova Kolinko <vovakolinko@gmail.com>
2022-01-22 15:50:20 +0300

    Добавлена функция для создания временной папки

--- libs.py
+++ libs.py
@@ -1,1 +1,15 @@
+from http.client import OK
 import os
+
+# Функция для создания временной папки
+def createtmppath(path):
+    # Проверка на наличие
+    if not os.path.isdir(path):
+        # если её нет - создаём
+        os.system('mkdir '+str(path))
+        # Возвращаем True
+        return True
+    # если она есть
+    # Возвращаем True
+    return True
+
