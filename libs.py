2e7c2157e67f13f34b180e6ed759af055c009304
Vova Kolinko <vovakolinko@gmail.com>
2022-01-22 15:59:48 +0300

    Добавлена функция для создания/удаления временного файла

--- libs.py
+++ libs.py
@@ -11,3 +11,18 @@ def createtmppath(path):
     # если она есть
     # Возвращаем path
     return path
+
+# Функция для создания/удаления временного файла
+def toggletempfile(status,path):
+    # Если нужно создать
+    if status == 'lock':
+        # Создаём
+        os.system('echo "lock" >' +path+'/status.txt')
+    # Если нужно удалить
+    elif status == 'unlock':
+        # Проверка на существование
+        if os.path.exists(path+'/status.txt'):
+            # Удаление
+            os.system('rm '+path+'/status.txt')
+    # Возвращение True
+    return True
